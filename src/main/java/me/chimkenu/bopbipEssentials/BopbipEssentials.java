bd2da4690109d9e4a98dfbb4277cb76f5acb22cb
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-12-07 13:36:06 +1100

    Initial commit

---
+++ src/main/java/me/chimkenu/bopbipEssentials/BopbipEssentials.java
@@ -0,0 +1,10 @@
+package me.chimkenu.bopbipEssentials;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BopbipEssentials implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/chimkenu/bopbipEssentials/client/BopbipEssentialsClient.java
@@ -0,0 +1,10 @@
+package me.chimkenu.bopbipEssentials.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BopbipEssentialsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
